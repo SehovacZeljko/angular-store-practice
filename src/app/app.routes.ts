fdb912f20ab78ad4c3996c739410b07bfc5f3b9a
Zeljko_Sehovac <zeljko.sehovac@singularity.is>
2025-11-25 16:30:29 +0100

    Redirect landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'Home' },
   { path: 'home', component: HomeComponent, title: 'Home' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
